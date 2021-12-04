5a0aa85ebd087dc8082500f5c148607aa672a454
Skyler H <skylerh15@users.noreply.github.com>
2021-12-04 00:24:14 -0600

    Added T11 tokens
    removed mop stuff

--- RCArmorTokenData.lua
+++ RCArmorTokenData.lua
@@ -6,159 +6,6 @@
 RCTokenTable = {
 	--[xxxxxx] = "ExampleSlot",
 
-	-- TIER 16	 Siege of Orgrimmar ---------------------------
-		-- Flexible
-	[99742] = "ChestSlot",
-	[99743] = "ChestSlot",
-	[99744] = "ChestSlot",
-
-	[99751] = "LegsSlot",
-	[99752] = "LegsSlot",
-	[99753] = "LegsSlot",
-	
-	[99748] = "HeadSlot",
-	[99749] = "HeadSlot",
-	[99750] = "HeadSlot",
-	
-	[99754] = "ShoulderSlot",
-	[99755] = "ShoulderSlot",
-	[99756] = "ShoulderSlot",
-	
-	[99745] = "HandsSlot",
-	[99746] = "HandsSlot",
-	[99747] = "HandsSlot",
-
-
-		-- Normal
-	[99696] = "ChestSlot",
-	[99691] = "ChestSlot",
-	[99686] = "ChestSlot",
-
-	[99693] = "LegsSlot",
-	[99684] = "LegsSlot",
-	[99688] = "LegsSlot",
-
-	[99683] = "HeadSlot",
-	[99689] = "HeadSlot",
-	[99694] = "HeadSlot",
-
-	[99690] = "ShoulderSlot",
-	[99685] = "ShoulderSlot",
-	[99695] = "ShoulderSlot",
-
-	[99682] = "HandsSlot",
-	[99687] = "HandsSlot",
-	[99692] = "HandsSlot",
-
-		-- Heroic
-	[99714] = "ChestSlot",
-	[99715] = "ChestSlot",
-	[99716] = "ChestSlot",
-
-	[99712] = "LegsSlot",
-	[99726] = "LegsSlot",
-	[99713] = "LegsSlot",
-
-	[99723] = "HeadSlot",
-	[99724] = "HeadSlot",
-	[99725] = "HeadSlot",
-
-	[99717] = "ShoulderSlot",
-	[99718] = "ShoulderSlot",
-	[99719] = "ShoulderSlot",
-
-	[99720] = "HandsSlot",
-	[99721] = "HandsSlot",
-	[99722] = "HandsSlot",
-
-
-	-- TIER 15	 Throne of Thunder ----------------------------
-		-- Normal
-	[95569] = "ChestSlot",
-	[95574] = "ChestSlot",
-	[95579] = "ChestSlot",
-
-	[95576] = "LegsSlot",
-	[95572] = "LegsSlot",
-	[95581] = "LegsSlot",
-
-	[95571] = "HeadSlot",
-	[95577] = "HeadSlot",
-	[95582] = "HeadSlot",
-
-	[95573] = "ShoulderSlot",
-	[95578] = "ShoulderSlot",
-	[95583] = "ShoulderSlot",
-
-	[95575] = "HandsSlot",
-	[95570] = "HandsSlot",
-	[95580] = "HandsSlot",
-
-		-- Heroic
-	[96566] = "ChestSlot",
-	[96567] = "ChestSlot",
-	[96568] = "ChestSlot",
-
-	[96631] = "LegsSlot",
-	[96632] = "LegsSlot",
-	[96633] = "LegsSlot",
-
-	[96623] = "HeadSlot",
-	[96624] = "HeadSlot",
-	[96625] = "HeadSlot",
-
-	[96699] = "ShoulderSlot",
-	[96700] = "ShoulderSlot",
-	[96701] = "ShoulderSlot",
-
-	[96599] = "HandsSlot",
-	[96600] = "HandsSlot",
-	[96601] = "HandsSlot",
-
-
-	-- TIER 14	- Heart of Fear, Terreace of Endless Springs---------------------
-		--Normal
-	[89238] = "ChestSlot",
-	[89237] = "ChestSlot",
-	[89239] = "ChestSlot",
-
-	[89243] = "LegsSlot",
-	[89244] = "LegsSlot",
-	[89245] = "LegsSlot",
-
-	[89234] = "HeadSlot",
-	[89235] = "HeadSlot",
-	[89236] = "HeadSlot",
-
-	[89246] = "ShoulderSlot",
-	[89247] = "ShoulderSlot",
-	[89247] = "ShoulderSlot",
-
-	[89240] = "HandsSlot",
-	[89241] = "HandsSlot",
-	[89242] = "HandsSlot",
-		--Heroic
-	[89250] = "ChestSlot",
-	[89249] = "ChestSlot",
-	[89251] = "ChestSlot",
-
-	[89252] = "LegsSlot",
-	[89253] = "LegsSlot",
-	[89254] = "LegsSlot",
-
-	[89258] = "HeadSlot",
-	[89260] = "HeadSlot",
-	[89259] = "HeadSlot",
-
-	[89261] = "ShoulderSlot",
-	[89262] = "ShoulderSlot",
-	[89263] = "ShoulderSlot",
-
-	[89255] = "HandsSlot",
-	[89256] = "HandsSlot",
-	[89257] = "HandsSlot",
-
-
 	-- TIER 13 - Dragon Soul ---------------------------------
 		-- Normal
 	[78174] = "ChestSlot",
@@ -231,6 +78,38 @@ RCTokenTable = {
 	[71669] = "HandsSlot",
 	[71676] = "HandsSlot",
 	[71683] = "HandsSlot",
+
+	
+	-- TIER 11 -------------------------
+		--Normal  (Some tiers were bought for JP)
+	[63683] = "HeadSlot",
+	[63684] = "HeadSlot",
+	[63682] = "HeadSlot",
+
+	[64315] = "ShoulderSlot",
+	[64316] = "ShoulderSlot",
+	[64314] = "ShoulderSlot",
+
+		--Heroic
+	[67423] = "ChestSlot",
+	[67424] = "ChestSlot",
+	[67425] = "ChestSlot",
+
+	[67428] = "LegsSlot",
+	[67427] = "LegsSlot",
+	[67426] = "LegsSlot",
+
+	[65001] = "HeadSlot",
+	[65000] = "HeadSlot",
+	[65002] = "HeadSlot",
+
+	[65088] = "ShoulderSlot",
+	[65087] = "ShoulderSlot",
+	[65089] = "ShoulderSlot",
+
+	[67429] = "HandsSlot",
+	[67430] = "HandsSlot",
+	[67431] = "HandsSlot",
 
 
 	 -- TIER 8 - Ulduar -------------------------------
