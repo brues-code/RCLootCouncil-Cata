6d4a3c771beceb026b48d32f45bd61f22f0aac65
Skyler H <skylerh15@users.noreply.github.com>
2021-12-05 07:43:59 -0600

    UnitIsGroupLeader compat

--- RCLootCouncil/compat.lua
+++ RCLootCouncil/compat.lua
@@ -16,3 +16,21 @@ function addon:GetNumGroupMembers()
         return GetNumPartyMembers()
     end
 end
+
+function addon:UnitIsGroupLeader(unit)
+	local unitName = UnitName(unit)
+	if unit == "player" or unitName == UnitName("player") then
+		return (IsRaidLeader() or IsPartyLeader()) == 1
+	end
+	if self:IsInRaid() then
+		for i = 1, MAX_RAID_MEMBERS do
+			local name, rank = GetRaidRosterInfo(i)
+			if name == unitName then
+				return rank == 2
+			end
+		end
+	elseif self:IsInGroup() then
+		local leaderIndex = GetPartyLeaderIndex()
+		return unitName == UnitName("party"..leaderIndex)
+	end
+end
